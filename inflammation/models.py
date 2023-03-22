10d0222d7a31fb2d3c4463697982ae038abc8023
gpederzani <giulia.pederzani@protonmail.com>
2023-03-22 10:06:50 +0100

    Add function patient_normalise

--- inflammation/models.py
+++ inflammation/models.py
@@ -43,3 +43,9 @@ def daily_min(data):
     :return: array of minimum measurements (across patients) of inflammation data
     """
     return np.min(data, axis=0)
+
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    max = np.max(data, axis=0)
+    return data / max[:, np.newaxis]
