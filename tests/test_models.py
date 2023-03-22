f837814377d3596b68e0970b1a57a97db17af168
gpederzani <giulia.pederzani@protonmail.com>
2023-03-22 10:00:48 +0100

    Code style fixes

--- tests/test_models.py
+++ tests/test_models.py
@@ -31,7 +31,8 @@ def test_daily_mean_integers():
 
 
 import pytest
-...
+
+
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
     from inflammation.models import daily_min
@@ -71,11 +72,10 @@ def test_daily_max(test, expected):
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
     ])
 def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
-
